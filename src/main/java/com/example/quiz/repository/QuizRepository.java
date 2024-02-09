4f85a768dc864a63308cba692edaf8271b0e37c7
tech-kind <j149335.oku@gmail.com>
2024-02-09 17:51:07 +0900

    first commit

---
+++ src/main/java/com/example/quiz/repository/QuizRepository.java
@@ -0,0 +1,11 @@
+package com.example.quiz.repository;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.quiz.entity.Quiz;
+
+public interface QuizRepository extends CrudRepository<Quiz, Integer> {
+    @Query("SELECT id FROM quiz ORDER BY RANDOM() limit 1")
+    Integer getRandomId();
+}
